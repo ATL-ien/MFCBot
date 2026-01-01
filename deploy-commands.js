b7a85ff0891d29b1422e408084baa098ca320904
ATL-ien <109189488+ATL-ien@users.noreply.github.com>
2025-12-31 21:12:59 -0500

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -30,7 +30,7 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
     console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
     // The put method is used to fully refresh all commands in the guild with the current set
-    const data = await rest.put(Routes.applicationCommands(process.env.CLIENTID), {
+    const data = await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
       body: commands,
     });
 
